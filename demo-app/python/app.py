0b09e077332171f2f68e9c7d159d3e147c4c41a1
Alex Berber <berber.it@gmail.com>
2022-02-06 21:15:41 +0200

    demo loader is ready

--- demo-app/python/app.py
+++ demo-app/python/app.py
@@ -1,7 +1,9 @@
-from multiprocessing import Pool
+# from multiprocessing import Pool
 import os
 from flask import Flask
 from dotenv import load_dotenv, find_dotenv
+from pathos.multiprocessing import ProcessingPool as Pool
+from threading import Thread
 
 load_dotenv(find_dotenv())
 APP_PORT = os.environ.get('APP_PORT')
@@ -10,29 +12,54 @@ DEBUG = os.environ.get('DEBUG')
 
 app = Flask(__name__)
 
-def make_load():
-    def f(x):
-        # Put any cpu (only) consuming operation here. I have given 1 below -
-        while True:
-            x * x
 
-    # decide how many cpus you need to load with.
-    no_of_cpu_to_be_consumed = 3
+class Loader:
 
-    p = Pool(processes=no_of_cpu_to_be_consumed)
-    p.map(f, range(no_of_cpu_to_be_consumed))
+    @staticmethod
+    def create_file():
+        f = open("file.txt", "a")
+        f.write("Now the file has more content!")
+        f.close()
 
-@app.route('/', methods = ['GET'])
+    @staticmethod
+    def del_file():
+        if os.path.exists("file.txt"):
+            os.remove("file.txt")
+
+    @staticmethod
+    def make_load():
+        def f(x):
+            # Put any cpu (only) consuming operation here. I have given 1 below -
+            while True:
+                if not os.path.exists("file.txt"):
+                    return
+                x * x
+
+        # decide how many cpus you need to load with.
+        no_of_cpu_to_be_consumed = 3
+
+        p = Pool(processes=no_of_cpu_to_be_consumed)
+        p.map(f, range(no_of_cpu_to_be_consumed))
+
+
+@app.route('/', methods=['GET'])
 def george():
     return '1984'
 
-@app.route('/start', methods = ['GET', 'POST'])
+
+@app.route('/start', methods=['GET'])
 def start_load():
-    make_load()
+    def start_balagan():
+        Loader.create_file()
+        Loader.make_load()
+
+    Thread(target = start_balagan).start()
     return 'Kick my grandma in the ankle!'
 
-@app.route('/stop', methods = ['GET', 'POST'])
+
+@app.route('/stop', methods=['GET'])
 def stop_load():
+    Loader.del_file()
     return 'No Fate...'
 
 
